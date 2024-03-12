6bdaaf6235a7129c825dfd8a3edb2e9265e6aff4
Don <don@castify.ai>
2024-03-12 19:03:42 +0300

    added fs

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 
 const firstArray = require('./data-short.js');
 const final = require('./final.js');
+const fs = require('fs');
 
 firstArray.forEach(item=> item.event_description = final[item.event]);
 console.log(firstArray);
