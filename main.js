c2a74ef6969e58e14b1301598cad8a07963d2598
Don <don@castify.ai>
2024-03-13 08:35:17 +0300

    importing file and adding event description

--- main.js
+++ main.js
@@ -4,7 +4,10 @@ const final = require('./final.js');
 const fs = require('fs');
 
 firstArray.forEach(item=> item.event_description = final[item.event]);
-console.log(firstArray);
-
-const strArray = firstArray.toString();
+console.log(firstArray)
+const strArray = firstArray.map(item => `Event: ${item.event}, Description; ${item.event_description}`).join('\n');
+//const strArray = firstArray.toString();
+fs.writeFile("updated.js", strArray, (err) =>{
+console.log(strArray)
+})
 
---
+++ updated.js
@@ -0,0 +1,5 @@
+Event: 31, Description; AD_BREAK_OPPORTUNITY
+Event: 40, Description; PAGE_CLICK
+Event: 40, Description; PAGE_CLICK
+Event: 31, Description; AD_BREAK_OPPORTUNITY
+Event: 28, Description; AD_COMPLETE
